abf97678475017822e4c398a4b09cb4203d08736
AleksanderWWW <alwojnarowicz@gmail.com>
2023-11-22 18:13:27 +0100

    sync not needed

--- tests/unit/test_default_run_name.py
+++ tests/unit/test_default_run_name.py
@@ -21,12 +21,10 @@ from neptune.exceptions import MissingFieldException
 
 def test_default_run_name():
     with neptune.init_run(mode="debug") as run:
-        run.sync()
         sys_id = run["sys/id"].fetch()
         assert sys_id is not None
         with pytest.raises(MissingFieldException):
             run["sys/name"].fetch()
 
     with neptune.init_run(name="Something", mode="debug") as run2:
-        run2.sync()
         assert run2["sys/name"].fetch() == "Something"
