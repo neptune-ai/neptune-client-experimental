0fd9c1d70e9e7be9a62e1f59a7f124f6455cc28a
AleksanderWWW <alwojnarowicz@gmail.com>
2024-01-04 14:52:51 +0100

    fix tests

--- tests/unit/test_custom_run_id_len.py
+++ tests/unit/test_custom_run_id_len.py
@@ -13,23 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from unittest.mock import (
-    Mock,
-    patch,
-)
-
 from faker import Faker
-from neptune import init_run
-from neptune.internal.backends.neptune_backend_mock import NeptuneBackendMock
+from neptune.internal.utils.limits import custom_run_id_exceeds_length
 
 fake = Faker()
 
 
-@patch("neptune.metadata_containers.metadata_container.get_backend")
-def test_max_length_custom_run_id(get_backend_mock):
-    get_backend_mock.return_value = Mock(wraps=NeptuneBackendMock())
-
+def test_max_length_custom_run_id():
     custom_run_id = "".join(fake.random_letters(128))
-    with init_run(custom_run_id=custom_run_id) as exp:
-        _, kwargs = exp._backend.create_run.call_args
-        assert kwargs["custom_run_id"] == custom_run_id
+    assert not custom_run_id_exceeds_length(custom_run_id)
