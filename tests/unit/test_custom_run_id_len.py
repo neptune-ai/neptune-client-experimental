b48ee6944a60cb73b3bfab1b9e0ce94e2147c626
AleksanderWWW <alwojnarowicz@gmail.com>
2024-01-08 14:32:25 +0100

    custom run id len to 128 (#46)
    Co-authored-by: Rafal Jankowski <rafal.jankowski@neptune.ai>

---
+++ tests/unit/test_custom_run_id_len.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2023, Neptune Labs Sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from faker import Faker
+from neptune.internal.utils.limits import custom_run_id_exceeds_length
+
+fake = Faker()
+
+
+def test_max_length_custom_run_id():
+    custom_run_id = "".join(fake.random_letters(128))
+    assert not custom_run_id_exceeds_length(custom_run_id)
+    custom_run_id = "".join(fake.random_letters(129))
+    assert custom_run_id_exceeds_length(custom_run_id)
